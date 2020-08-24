7e75904a7a649ac617571da3089b9408401ea5c4
smc181002 <meherchaitanya18802@gmail.com>
2020-08-24 17:26:55 +0530

    Updates

---
+++ precache-manifest.f4d96540d229661d9a3bc73ddda2305d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46b5e8472d275669a1e1a68073e0cbf9",
+    "url": "/dev/index.html"
+  },
+  {
+    "revision": "7185f75d2d699c2a6393",
+    "url": "/dev/static/css/main.f45d9226.chunk.css"
+  },
+  {
+    "revision": "d0fec47d93d895ace977",
+    "url": "/dev/static/js/2.6e2c7cd7.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/dev/static/js/2.6e2c7cd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7185f75d2d699c2a6393",
+    "url": "/dev/static/js/main.25040bf2.chunk.js"
+  },
+  {
+    "revision": "45dbeab8c02b91a05348",
+    "url": "/dev/static/js/runtime-main.a36dba7b.js"
+  }
+]);
